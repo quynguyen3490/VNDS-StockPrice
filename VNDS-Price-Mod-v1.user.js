1c948d1e634719ac9692d0e9d4fc6f7ac3751ea7
Quy Nguyen <quynguyen3490@gmail.com>
2021-09-13 15:45:05 +0700

    Update VNDS-Price-Mod-v1.user.js

--- VNDS-Price-Mod-v1.user.js
+++ VNDS-Price-Mod-v1.user.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  try to take over the world!
 // @author       QuyNguyen
-// @match        https://trade.vndirect.com.vn/chung-khoan/danh-muc
+// @match        https://trade.vndirect.com.vn/*
 // @icon         https://www.google.com/s2/favicons?domain=vndirect.com.vn
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
@@ -12,10 +12,11 @@
 // ==/UserScript==
 
 $(document).ready(function() {
+    $("<style type='text/css'> .highlight_quy{ background-color:#ffd900; } </style>").appendTo("head");
     setInterval(updateinfo, 5000);
 
     $('ul.footer-quick-nav').click(function(){
-        var delayInMilliseconds = 300; //1 second
+        var delayInMilliseconds = 200; //1 second
         setTimeout(function() {
             updateinterface();
             updateinfo();
@@ -34,30 +35,35 @@ function updateinterface(){
         'border-radius': '10px'});
     $('table.category-list thead').find('th').eq(4).after('<th class="tc">Lãi/lỗ</th>');
     $('.right-frame').draggable();
-    $('.right-frame').resizable();
 };
 
 function updateinfo(){
+    var change = false;
     $('.loinhuanvnd').remove();
     $('table.category-list tbody').find('tr').each(function(){
-                var slcp = $(this).find('td').eq(2).html();
-                var price = $(this).find('td').eq(3).html();
-                var percent = $(this).find('td').eq(4).find('span').html();
-                slcp = parseFloat(slcp.replace(',',''));
-                price = parseFloat(price.replace(',',''));
-                percent = percent.replace(/\<.*?\>/g,'');
-                percent = percent.replace('%','');
-                percent = parseFloat(percent);
-                var profit = slcp*price*percent/100;
+        var slcp = $(this).find('td').eq(2).html();
+        var price = $(this).find('td').eq(3).html();
+        var percent = $(this).find('td').eq(4).find('span').html();
+        slcp = parseFloat(slcp.replace(',',''));
+        price = parseFloat(price.replace(',',''));
+        percent = percent.replace(/\<.*?\>/g,'');
+        percent = percent.replace('%','');
+        percent = parseFloat(percent);
 
-                var color = '';
-                if(profit<0) {
-                    color = 'txt-pink';
-                }else if(profit==0){
-                   color = 'txt-gia-tc';
-                }else{
-                    color = 'txt-lime';
-                };
-                $(this).find('td').eq(4).after('<td class="tr loinhuanvnd"><span class="'+color+'">' + new Intl.NumberFormat().format(profit) + '</span></td>');
-     });
+        var profit = slcp*price*percent/100;
+
+        var color = '';
+        if(profit<0) {
+            color = 'txt-pink';
+        }else if(profit==0){
+            color = 'txt-gia-tc';
+        }else{
+            color = 'txt-lime';
+        };
+        $(this).find('td').eq(4).after('<td class="tr loinhuanvnd"><span class="'+color+'">' + new Intl.NumberFormat().format(profit) + '</span></td>');
+    });
+    $('.loinhuanvnd').addClass('highlight_quy');
+    setTimeout(function(){
+        $('.loinhuanvnd').removeClass('highlight_quy');
+    },800);
 };
